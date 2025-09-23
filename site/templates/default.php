f31ed7a249082f76b0eef49c82553043c560345c
Jorge Fco.™ <12586457+jorge-fco@users.noreply.github.com>
2025-09-23 14:30:15 -0600

    Update default.php

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,1 +1,15 @@
-<h1><?= $page->title() ?></h1>
+<?php snippet('header') ?>
+<?php snippet('navigation') ?>
+
+<main class="">
+	<section class="">
+		<div class="" role="layout">
+			<div class="">
+				<h1><?= $page->title(); ?></h1>
+			</div>
+		</div>
+	</section>
+</main>
+
+<?php snippet('footer') ?>
+<?php snippet('footer.code') ?>
