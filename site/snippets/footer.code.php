f2fa9cf3fc03c6e97765c3831d2b0a2d0e8610f3
Jorge Fco.™ <12586457+jorge-fco@users.noreply.github.com>
2026-04-16 16:36:06 -0600

    Create footer.code.php

---
+++ site/snippets/footer.code.php
@@ -0,0 +1,7 @@
+<!--JAVASCRIPT-->
+<script src="https://code.jquery.com/jquery-4.0.0.min.js" integrity="sha256-OaVG6prZf4v69dPg6PhVattBXkcOWQB62pdZ3ORyrao=" crossorigin="anonymous"></script>
+<?= js('assets/dist/vendors.min.js?v='.rand(1000, 5000)) ?>
+<?= js('assets/dist/app.min.js?v='.rand(1000, 4000)) ?>
+
+</body>
+</html>
